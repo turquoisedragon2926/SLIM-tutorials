7045ea98580bb21fe8ad1e6e739bea8b4a947877
Grant Bruer <grant.git.email@gmail.com>
2024-11-15 07:36:28 -0500

    Set compat entries and removed NormalizingFlowFilters

--- 05-Digital-Twins/src/estimator.jl
+++ 05-Digital-Twins/src/estimator.jl
@@ -1,34 +1,6 @@
 using LinearAlgebra: Diagonal
 using EnsembleKalmanFilters: EnKF
-using NormalizingFlowFilters:
-    cpu,
-    ConditionalGlowOptions,
-    NetworkConditionalGlow,
-    OptimizerOptions,
-    create_optimizer,
-    TrainingOptions,
-    NormalizingFlowFilter
 using Configurations: from_dict
-
-function Ensembles.assimilate_data(
-    filter::NormalizingFlowFilter,
-    ensemble,
-    ensemble_obs_clean,
-    ensemble_obs_noisy,
-    y_obs,
-    log_data,
-)
-    X_matrix = NormalizingFlowFilters.assimilate_data(
-        filter,
-        Float64.(get_ensemble_matrix(ensemble)),
-        Float64.(get_ensemble_matrix(ensemble_obs_noisy)),
-        get_member_vector(ensemble_obs_clean, y_obs),
-        log_data,
-    )
-    members = get_ensemble_dicts(ensemble, X_matrix)
-    posterior = Ensemble(members, ensemble.state_keys)
-    return posterior
-end
 
 function get_noise_covariance(params::NoiseOptions)
     type = params.type
